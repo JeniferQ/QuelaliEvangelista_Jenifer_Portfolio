d11fc096f30c49c49a4ebdc53489d0f36be251d8
JeniferQ <144718154+JeniferQ@users.noreply.github.com>
2024-10-20 02:25:34 -0400

    Add toggle menu
    In this commit, the mobile menu and menu animations were added through the use of classes.

---
+++ js/main.js
@@ -0,0 +1,37 @@
+(() => {
+    const burger = document.querySelector('#burger img'),
+    menu = document.querySelector('#menu'),
+    body = document.querySelector('body');
+
+    function removeMenu() {
+        body.style.overflow = 'auto';
+        menu.classList.remove('open');
+        burger.classList.remove('rotate');
+    }
+        
+    function toggleMenu() {
+        if (window.innerWidth <= 768) {
+            menu.classList.toggle('open');
+            burger.classList.toggle('rotate');
+        
+            if (menu.classList.contains('open')) {
+                body.style.overflow = 'hidden';
+            }
+
+            else {
+                console.log('close mobile menu');
+                removeMenu(); }
+        }
+    }
+    
+    function resizeMenu() {
+        if (window.innerWidth >= 768) {
+            removeMenu();
+        }
+    }
+
+    burger.addEventListener('click', toggleMenu);
+    window.addEventListener('resize', resizeMenu);
+})();
+
+const player = new Plyr('video');
